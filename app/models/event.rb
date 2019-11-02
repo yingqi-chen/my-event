ec503d01884ba14d6bc935cb1d1ea4105122bfe9
chanwkkk <chanwk18@hotmail.com>
2019-11-02 15:49:05 -0600

    modify model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
     belongs_to :organization, class_name: "User"
-    has_many :volunteers, through: :comments, source: :volunteer
+    has_many :volunteers, through: :comments, source: "Volunteer"
     has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :organized_events, foreign_key: "organization_id", class_name: "Event"
-    has_many :joined_events, through: :being_commented_comments, foreign_key: "volunteer_id", source:"Event"
+    has_many :joined_events, through: :being_commented_comments, :source => :event
     has_many :commented_comments, foreign_key: "organization_id", class_name: "Comment"
     has_many :being_commented_comments, foreign_key: "volunteer_id", class_name: "Comment"
 end
