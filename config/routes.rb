a7e4dc75ac2b8c4c6c1e5d0c934ab2575eb9643b
chanwkkk <chanwk18@hotmail.com>
2019-11-06 23:38:19 -0700

    set up the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :events, :users, :comments
+  
   resources :events, only: [:show] do 
-    resources :comments 
+    resources :comments, only: [:show] 
   end
-
+  
+  resources :users, only:[:show] do 
+    resources :events
+  end
   
 end
