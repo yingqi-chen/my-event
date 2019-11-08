bba06f4d300d501b9a98994c0eb1f680d6ec4c08
chanwkkk <chanwk18@hotmail.com>
2019-11-08 00:14:31 -0700

    add welcome page and add some session routes and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root 'static#home'
+
+  get '/login', to: "session#new", as: "login"
+  get '/logout', to: "session#destroy", as: "logout"
+  
   resources :events, :users, :comments
   
   resources :events, only: [:show] do 
