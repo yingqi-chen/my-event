03d3ef0e1c1ed5f6057983a4d06967c752434071
chanwkkk <chanwk18@hotmail.com>
2019-12-04 11:26:56 -0700

    add an validator for comment on volunteer

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,10 +1,11 @@
 class Comment < ApplicationRecord
     validate :comment_made_by_organization, on: :create
+    validate :comment_on_volunteer, on: :create
+
 
     belongs_to :organization, class_name: "User"
     belongs_to :volunteer, class_name: "User"
     belongs_to :event
-
     validates :organization_id, :volunteer_id, :event_id, presence: true
     validates :content, length:{minimum: 20}
     
@@ -13,5 +14,11 @@ class Comment < ApplicationRecord
        unless self.organization.is_organization
         errors.add(:organization_id, "has to be of an organization.")
        end
+    end
+
+    def comment_on_volunteer
+        if self.volunteer.is_organization
+            errors.add(:volunteer_id, "has to be of an volunteer")
+        end
     end
 end
