2eda8e5c8f01dbf809681256a34176e22c120733
chanwkkk <chanwk18@hotmail.com>
2019-12-07 01:08:58 -0700

    validate event show page and add displaying errors in layout doc

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,9 +2,26 @@ class EventsController < ApplicationController
     before_action :find_event, only: [:show, :edit]
     
     def find_event
+      @event = Event.find_by :id=>params[:id] 
+      unless @event
+        flash[:error] = "There is no such event."
+        redirect_to '/events'
+      end
     end
 
     def index
         @events = Event.all
+    end
+
+    def edit
+        @user = @event.organization
+        authorized_user?
+    end
+
+    def show
+
+    end
+
+    def update
     end
 end
