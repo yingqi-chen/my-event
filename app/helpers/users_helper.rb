96641e22e27bcead6862c06a69850a5555c482b8
chanwkkk <chanwk18@hotmail.com>
2019-12-12 01:29:20 -0700

    modify user show page using is_organization_field helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+
+    def is_organization_field?(user,comments)
+        if user.is_organization 
+          "As an organization, #{user.name} makes #{comments.size} comment(s)" 
+        else
+          "As an volunteer, #{user.name} receives #{comments.size} comment(s)" 
+        end 
+    end
+
 end
+
+
